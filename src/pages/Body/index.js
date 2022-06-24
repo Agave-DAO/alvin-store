0ede207fe31a186126dd1eeb031329fdd9e3149f
Rodrigo Casale <rodrigocasale@Rodrigos-MacBook-Pro.local>
2022-06-24 11:32:08 -0500

    alvin header fix

--- src/pages/Body/index.js
+++ src/pages/Body/index.js
@@ -26,7 +26,7 @@ export function Header({ totalSupply, ready, balanceSOCKS, setShowConnect }) {
           <span role="img" aria-label="unicorn">
             🦄
           </span>{' '}
-          Unisocks
+          Alvin
         </Unicorn>
       </Link>
       <div style={{ display: 'flex', flexDirection: 'row' }}>
@@ -43,7 +43,7 @@ export function Header({ totalSupply, ready, balanceSOCKS, setShowConnect }) {
         <Account onClick={() => handleAccount()} balanceSOCKS={balanceSOCKS}>
           {account ? (
             balanceSOCKS > 0 ? (
-              <SockCount>{balanceSOCKS && `${amountFormatter(balanceSOCKS, 18, 0)}`} SOCKS</SockCount>
+              <SockCount>{balanceSOCKS && `${amountFormatter(balanceSOCKS, 18, 0)}`} ALVIN</SockCount>
             ) : (
               <SockCount>{account.slice(0, 6)}...</SockCount>
             )
