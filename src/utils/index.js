07a8f5ff6bcb9ea481631902baba305618d5fa96
Rodrigo Casale <rodrigocasale@Rodrigos-MacBook-Pro.local>
2022-06-27 08:41:22 -0500

    uniswapv2 contract ABI added

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,17 +11,7 @@ const FACTORY_ADDRESS = '0xA818b4F111Ccac7AA31D0BCc0806d64F2E0737D7'
 export const TOKEN_ADDRESSES = {
   ETH: 'ETH',
   SOCKS: '0x59715D8d206b3D4748Cec55E7c2DE26F23AF45D5',
-  ANT: '0x960b236A07cf122663c4303350609A66A7B288C0',
-  BAT: '0x0D8775F648430679A709E98d2b0Cb6250d2887EF',
-  DAI: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-  KNC: '0xdd974D5C2e2928deA5F71b9825b8b646686BD200',
-  MKR: '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2',
-  RDN: '0x255Aa6DF07540Cb5d3d297f0D0D4D84cb52bc8e6',
-  REP: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
-  SNT: '0x744d70FDBE2Ba4CF95131626614a1763DF805B9E',
-  SPANK: '0x42d6622deCe394b54999Fbd73D108123806f6a18',
-  TUSD: '0x8dd5fbCe2F6a956C3022bA3663759011Dd51e73E',
-  ZRX: '0xE41d2489571d322189246DaFA5ebDe1F4699F498'
+  AGVE: '0x3a97704a1b25F08aa230ae53B352e2e72ef52843'
 }
 
 export const TOKEN_SYMBOLS = Object.keys(TOKEN_ADDRESSES).reduce((o, k) => {
@@ -77,7 +67,7 @@ export function getExchangeContract(exchangeAddress, library, account) {
 }
 
 export async function getTokenExchangeAddressFromFactory(tokenAddress, library, account) {
-  return getContract(FACTORY_ADDRESS, FACTORY_ABI, library, account).getExchange(tokenAddress)
+  return getContract(FACTORY_ADDRESS, FACTORY_ABI, library, account).getPair(tokenAddress, "0x3a97704a1b25F08aa230ae53B352e2e72ef52843")
 }
 
 // get the ether balance of an address
